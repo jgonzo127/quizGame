39b099f093a2a58a959e470cbfc39de267c96c5b
Jordan Gonzales <jgonzo127@gmail.com>
2015-05-02 22:38:28 -0700

    adjusted margins

---
+++ js/dropdown.js
@@ -0,0 +1,136 @@
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<script>(function($, Drupal, window, document, undefined) {
+$(document).ready( function() {
+/* ul = the various winners of every year,
+        i = the up/sideways arrows next to the winners years 
+        by Jordan Gonzales */
+
+$(".year-of-winners > h3").on("click", function () {
+    if ($("[class*='winners'] > li:visible")) {
+        $(this).parent().find("ul").children().slideToggle();
+        $(this).find("i").toggle();
+    }
+});
+ });
+})(jQuery, Drupal, window, document);
+</script>
+
+<div class="year-of-winners">
+     <h3><i class="fa fa-chevron-down" id="down2014" style= "display:none"></i><i class="fa fa-chevron-right", id="right2014"></i><a href = ""></a><span> 2014 Winners</span></h3>
+
+    <ul class="winners2014">
+        <li><a href="#aa2014">Ansel Adams Award</a></li>
+        <li><a href="#ca2014">Communication Award</a></li>
+        <li><a href="#diw2014">Denny and Ida Wilcher Award</a></li>
+        <li><a href="#ds2014">Distinguished Service Award</a></li>
+        <li><a href="#ew2014">Edgar Wayburn Award</a></li>
+        <li><a href="#jm2014">John Muir Award</a></li>
+        <li><a href="#jb2014">Joseph Barbosa Award</a></li>
+        <li><a href="#cm2014">International - The Chico Mendes Award</a></li>
+        <li><a href="#ec2014">International - Earthcare Award</a></li>
+        <li><a href="#rs2014">International - Raymond J. Sherwin Award</a></li>
+        <li><a href="#mp2014">Madelyn Pyeatt Award</a></li>
+        <li><a href="#ok2014">Oliver Kehrlein Award</a></li>
+        <li><a href="#rb2014">Robert Bullard Environmental Justice Award</a></li>
+        <li><a href="#sa2014">Special Achievement Award</a></li>
+        <li><a href="#ss2014">Special Service Award</a></li>
+        <li><a href="#sm2014">Susan E. Miller Award</a></li>
+        <li><a href="#ws2014">Walter A. Starr Award</a></li>
+    </ul>
+</div>
+<div class="year-of-winners">
+     <h3><i class="fa fa-chevron-down" id="down2013" style= "display:none"></i><i class="fa fa-chevron-right" id="right2013" ></i><a href = ""></a><span> 2013 Winners</span></h3>
+
+    <ul class="winners2013">
+        <li><a href="#aa2013">Ansel Adams Award</a></li>
+        <li><a href="#ca2013">Communication Award</a></li>
+        <li><a href="#db2013">David Brower Award</a></li>
+        <li><a href="#diw2013">Denny and Ida Wilcher Award</a></li>
+        <li><a href="#ds2013">Distinguished Service Award</a></li>
+        <li><a href="#ew2013">Edgar Wayburn Award</a></li>
+        <li><a href="#fpf2013">Francis P. Farquhar Mountaineering Award</a></li>
+        <li><a href="#jm2013">John Muir Award</a></li>
+        <li><a href="#jb2013">Joseph Barbosa Award</a></li>
+        <li><a href="#cm2013">International - The Chico Mendes Award</a></li>
+        <li><a href="#ea2013">International - Earthcare Award</a></li>
+        <li><a href="#rs2013">International - Raymond J. Sherwin Award</a></li>
+        <li><a href="#mp2013">Madelyn Pyeatt Award</a></li>
+        <li><a href="#ok2013">Oliver Kehrlein Award</a></li>
+        <li><a href="#oc2013">One-Club Award</a></li>
+        <li><a href="#sa2013">Special Achievement Award</a></li>
+        <li><a href="#ss2013">Special Service Award</a></li>
+        <li><a href="#wd2013">William O. Douglas Award</a></li>
+    </ul>
+</div>
+<div class="year-of-winners">
+     <h3><i class="fa fa-chevron-down" id="down2012" style= "display:none"></i><i class="fa fa-chevron-right" id="right2012" ></i><a href = ""></a><span> 2012 Winners</span></h3>
+
+    <ul class="winners2012">
+        <li><a href="#aa2012">Ansel Adams Award</a></li>
+        <li><a href="#ca2012">Communication Award</a></li>
+        <li><a href="#db2012">David Brower Award</a></li>
+        <li><a href="#da2012">Distinguished Achievement Award</a></li>
+        <li><a href="#ew2012">Edgar Wayburn Award</a></li>
+        <li><a href="#ea2012">Environmental Alliance Award</a></li>
+        <li><a href="#fpf2012">Francis P. Farquhar Mountaineering Award</a></li>
+        <li><a href="#jm2012">John Muir Award</a></li>
+        <li><a href="#jb2012">Joseph Barbosa Award</a></li>
+        <li><a href="#cm2012">International - The Chico Mendes Award</a></li>
+        <li><a href="#ea2012">International - Earthcare Award</a></li>
+        <li><a href="#rs2012">International - Raymond J. Sherwin Award</a></li>
+        <li><a href="#mp2012">Madelyn Pyeatt Award</a></li>
+        <li><a href="#oc2012">One-Club Award</a></li>
+        <li><a href="#sa2012">Special Achievement Award</a></li>
+        <li><a href="#ss2012">Special Service Award</a></li>
+        <li><a href="#sm2012">Susan E. Miller Award</a></li>
+        <li><a href="#ws2012">Walter A. Starr Award</a></li>
+    </ul>
+</div>
+<div class="year-of-winners">
+     <h3><i class="fa fa-chevron-down" id="down2011" style= "display:none"></i><i class="fa fa-chevron-right" id="right2011" ></i><a href = ""></a><span> 2011 Winners</span></h3>
+
+    <ul class="winners2011">
+        <li><a href="#aa2011">Ansel Adams Award</a></li>
+        <li><a href="#ca2011">Communication Award</a></li>
+        <li><a href="#db2011">David Brower Award</a></li>
+        <li><a href="#da2011">Distinguished Achievement Award</a></li>
+        <li><a href="#ds2011">Distinguished Service Award</a></li>
+        <li><a href="#ew2011">Edgar Wayburn Award</a></li>
+        <li><a href="#ea2011">Environmental Alliance Award</a></li>
+        <li><a href="#fpf2011">Francis P. Farquhar Mountaineering Award</a></li>
+        <li><a href="#jm2011">John Muir Award</a></li>
+        <li><a href="#jb2011">Joseph Barbosa Award</a></li>
+        <li><a href="#ea2011">International - Earthcare Award</a></li>
+        <li><a href="#rs2011">International - Raymond J. Sherwin Award</a></li>
+        <li><a href="#mp2011">Madelyn Pyeatt Award</a></li>
+        <li><a href="#ok2011">Oliver Kehrlein Award</a></li>
+        <li><a href="#sa2011">Special Achievement Award</a></li>
+        <li><a href="#ss2011">Special Service Award</a></li>
+        <li><a href="#sm2011">Susan E. Miller Award</a></li>
+        <li><a href="#ws2011">Walter A. Starr Award</a></li>
+        <li><a href="#wc2011">William E. Colby Award</a></li>
+        <li><a href="#wd2011">William O. Douglas Award</a></li>
+    </ul>
+</div>
+<div class="year-of-winners">
+     <h3><i class="fa fa-chevron-down" id="down2010" style= "display:none"></i><i class="fa fa-chevron-right" id="right2010"></i><a href = ""></a><span> 2010 Winners</span></h3>
+
+    <ul class="winners2010">
+        <li><a href="#aa2010">Ansel Adams Award</a></li>
+        <li><a href="#db2010">David Brower Award</a></li>
+        <li><a href="#diw2010">Denny and Ida Wilcher Award</a></li>
+        <li><a href="#ds2010">Distinguished Service Award</a></li>
+        <li><a href="#ew2010">Edgar Wayburn Award</a></li>
+        <li><a href="#ea2010">Environmental Alliance Award</a></li>
+        <li><a href="#jm2010">John Muir Award</a></li>
+        <li><a href="#jb2010">Joseph Barbosa Award</a></li>
+        <li><a href="#ea2010">International - Earthcare Award</a></li>
+        <li><a href="#rs2010">International - Raymond J. Sherwin Award</a></li>
+        <li><a href="#mp2010">Madelyn Pyeatt Award</a></li>
+        <li><a href="#ok2010">Oliver Kehrlein Award</a></li>
+        <li><a href="#oc2010">One-Club Award</a></li>
+        <li><a href="#sa2010">Special Achievement Award</a></li>
+        <li><a href="#ss2010">Special Service Award</a></li>
+        <li><a href="#sm2010">Susan E. Miller Award</a></li>
+    </ul>
+</div>
